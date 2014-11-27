4fa3e2e6bd00878cefad9e258113e2273b677528
Julian Lindblad <JulianLindblad@gmail.com>
2014-11-27 17:30:40 +0900

    Fixed secondary sorting.

--- controller.js
+++ controller.js
@@ -1,9 +1,15 @@
 
+
+// Taken from:
+// https://github.com/arantius/persona-fusion-calculator/blob/gh-pages/app.js
+//recipe.cost += (27 * level * level) + (126 * level) + 2147;
+
+
 var persona4App = angular.module('persona4App', []);
 
 persona4App.controller('testController', ['$scope', '$http', 
   function($scope, $http) {
-    $scope.orderProp = 'Lv';
+    $scope.orderProp = 'level';
     $scope.cur_window = 'list';
     
 
